8e3a3500a4783c28109818415ae4937d6fcc6063
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-06-20 09:49:19 +0200

    Added get account route

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getUserData } = require("../firebase/database/userData");
+const { auth, db } = require("../firebase/config/config");
 
 function isUser(req, res, next) {
   if (!auth.currentUser) {
@@ -15,10 +16,16 @@ router.get("/", isUser, async (req, res) => {
   const user = await getUserData(uid);
 
   if (!user) {
-    return res.status(400).json({ message: "User not found" });
+    return res.status(404).json({ message: "User not found" });
   }
 
-  res.status(200).json({ user });
+  const dbUser = await getUserData(uid);
+
+  if (!dbUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json({ dbUser });
 });
 
 module.exports = router;
